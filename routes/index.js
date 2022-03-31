18863ca28f0cd1ad5cc43da4a4069b9ec15d03ba
JovitoAddo <jovitoaddo@gmail.com>
2022-03-31 17:37:40 +0700

    update index router

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router()
 
 const instructorRouter = require("./instructor-routes")
+const participantsRouter = require("./participants-routes")
+const coursesRouter = require("./courses-routes")
 
 router.get("/", (req, res) => {
   res.json("selamat datang di mongo atlas");
 });
 
 router.use("/instructors", instructorRouter)
+router.use("/participants", participantsRouter)
+router.use("/courses", coursesRouter)
+
 
 
 module.exports = router
